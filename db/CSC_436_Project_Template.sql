afb94890c2e0bade326d3c9a92cee34549042e69
Matt Barbrack <mbarbrack@gmail.com>
2026-04-05 22:04:25 -0400

    Phase II write up

--- db/CSC_436_Project_Template.sql
+++ db/CSC_436_Project_Template.sql
@@ -36,13 +36,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `your_database_name`
---
-CREATE DATABASE IF NOT EXISTS `your_database_name` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `your_database_name`;
-
 -- --------------------------------------------------------
 
 --
